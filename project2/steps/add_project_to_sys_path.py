1ab4491d29b7a9144f15866a746aa76752f8d871
Udhayaraj Sivalingam <udhayaraj12@gmail.com>
2024-10-07 18:16:39 +0200

    common code reuse with dab sync

--- project2/steps/add_project_to_sys_path.py
+++ project2/steps/add_project_to_sys_path.py
@@ -16,6 +16,21 @@ def add_notebook_path_to_sys_path():
     sys.path.append(notebook_path)
 
 
+def add_project_files_path_to_sys_path():
+    project_files_path = "/Workspace" + os.path.dirname(os.path.dirname(
+        os.path.dirname(
+            dbutils.notebook.entry_point.getDbutils()
+            .notebook()
+            .getContext()
+            .notebookPath()
+            .get()
+        )
+    )
+    )
+    sys.path.append(project_files_path)
+
+
 # COMMAND ----------
 
 add_notebook_path_to_sys_path()
+add_project_files_path_to_sys_path()
